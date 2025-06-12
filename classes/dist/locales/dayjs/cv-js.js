efa47e0eb07eac00e495baf1796a308c1735b0c1
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-06-12 10:00:08 +0200

    fixes #76: Upgrade to Converse 11.0.1

--- classes/dist/locales/dayjs/cv-js.js
+++ classes/dist/locales/dayjs/cv-js.js
@@ -1,41 +1,40 @@
 (self["webpackChunkconverse_js"] = self["webpackChunkconverse_js"] || []).push([[2766],{
 
-/***/ 3871:
+/***/ 6083:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
-!function (_, e) {
-   true ? module.exports = e(__webpack_require__(5646)) : 0;
-}(this, function (_) {
-  "use strict";
-
-  function e(_) {
-    return _ && "object" == typeof _ && "default" in _ ? _ : {
-      default: _
+!function(_, e) {
+     true ? module.exports = e(__webpack_require__(2978)) : 0;
+}(this, function(_) {
+    "use strict";
+    function e(_) {
+        return _ && "object" == typeof _ && "default" in _ ? _ : {
+            default: _
+        };
+    }
+    var t = e(_), n = {
+        name: "cv",
+        weekdays: "вырсарникун_тунтикун_ытларикун_юнкун_кӗҫнерникун_эрнекун_шӑматкун".split("_"),
+        months: "кӑрлач_нарӑс_пуш_ака_май_ҫӗртме_утӑ_ҫурла_авӑн_юпа_чӳк_раштав".split("_"),
+        weekStart: 1,
+        weekdaysShort: "выр_тун_ытл_юн_кӗҫ_эрн_шӑм".split("_"),
+        monthsShort: "кӑр_нар_пуш_ака_май_ҫӗр_утӑ_ҫур_авн_юпа_чӳк_раш".split("_"),
+        weekdaysMin: "вр_тн_ыт_юн_кҫ_эр_шм".split("_"),
+        ordinal: function(_) {
+            return _;
+        },
+        formats: {
+            LT: "HH:mm",
+            LTS: "HH:mm:ss",
+            L: "DD-MM-YYYY",
+            LL: "YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ]",
+            LLL: "YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ], HH:mm",
+            LLLL: "dddd, YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ], HH:mm"
+        }
     };
-  }
-  var t = e(_),
-    n = {
-      name: "cv",
-      weekdays: "вырсарникун_тунтикун_ытларикун_юнкун_кӗҫнерникун_эрнекун_шӑматкун".split("_"),
-      months: "кӑрлач_нарӑс_пуш_ака_май_ҫӗртме_утӑ_ҫурла_авӑн_юпа_чӳк_раштав".split("_"),
-      weekStart: 1,
-      weekdaysShort: "выр_тун_ытл_юн_кӗҫ_эрн_шӑм".split("_"),
-      monthsShort: "кӑр_нар_пуш_ака_май_ҫӗр_утӑ_ҫур_авн_юпа_чӳк_раш".split("_"),
-      weekdaysMin: "вр_тн_ыт_юн_кҫ_эр_шм".split("_"),
-      ordinal: function (_) {
-        return _;
-      },
-      formats: {
-        LT: "HH:mm",
-        LTS: "HH:mm:ss",
-        L: "DD-MM-YYYY",
-        LL: "YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ]",
-        LLL: "YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ], HH:mm",
-        LLLL: "dddd, YYYY [ҫулхи] MMMM [уйӑхӗн] D[-мӗшӗ], HH:mm"
-      }
-    };
-  return t.default.locale(n, null, !0), n;
+    return t.default.locale(n, null, !0), n;
 });
+
 
 /***/ })
 
